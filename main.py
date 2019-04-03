e81c2072bf7c2e6b7c98625381d25f9efb26476e
Jeroen <jeroenvermazeren@outlook.com>
2019-04-03 11:08:04 +0200

    Changed structure of program

--- main.py
+++ main.py
@@ -11,68 +11,75 @@ from tensorflow import keras
 from IPython.display import Audio as audio_playback_widget
 import matplotlib.pyplot as plt
 
-seg = segmentation.Segmentation()
-cnn = cnn.Network()
-prefix = 'num'
-
-n_epochs = 20
-
-def preprocessing():
-    f = './data/raw-from-phone.wav'
-
-    samples, sample_rate = soundfile.read(f)
-
-    seg.show_waveform(samples, sample_rate)
-
-    # audio_playback_widget(f) unable because IPython
-
-    f = './data/num_phone_en-UK_m_Martin00.wav'
-
-    seg.show_spectogram(f)
-
-    seg.split_combined_file_into_wavs('./data/num_Bing_en-UK_f_Susan.wav')
-    seg.split_all_combined_files_into_wavs()
-
-    # stamp = seg.wav_to_stamp('num','six','phone_en-UK-m-Martin00.wav')
-
-    seg.create_dataset_from_folders('num')
-
-    seg.create_dataset_from_adhoc_wavs('num' + '-test')
+from cnn.network import Network
+from segmentation.segmetation import Segmentation
 
 
-def network():
-    network = cnn.build_network()
+class Main():
+    def __init__(self, segmentation, network):
+        self.segmentation = segmentation
+        self.network = network
+        self.prefix = 'num'
 
-    loss_function = 'categorical_crossentropy'
-    optimizer = keras.optimizers.Adam(lr = 0.001)
+        self.n_epochs = 20
 
-    network.compile(loss=loss_function, optimizer=optimizer, metrics=['accuracy'])
+    def preprocessing(self):
+        f = './data/raw-from-phone.wav'
 
-    dataset = pickle.load(open(os.path.join('data', prefix + '.pkl'),'rb'))
+        samples, sample_rate = soundfile.read(f)
 
+        self.segmentation.show_waveform(samples, sample_rate)
 
-    train_indices = [i for i, r in enumerate(dataset['rand']) if r <= 0.9]
-    check_indices = [i for i, r in enumerate(dataset['rand']) if r > 0.9]
+        # audio_playback_widget(f) unable because IPython
 
+        f = './data/num_phone_en-UK_m_Martin00.wav'
 
-    ds_train = cnn.make_dataset(dataset, train_indices,n_epochs = cnn.n_epochs, batch_size=cnn.batch_size, seed=100)  # shuffles...
-    ds_check = cnn.make_dataset(dataset, check_indices,n_epochs=cnn.n_epochs, batch_size=1)
+        self.segmentation.show_spectogram(f)
 
-    spe = len(train_indices) // cnn.batch_size
+        self.segmentation.split_combined_file_into_wavs('./data/num_Bing_en-UK_f_Susan.wav')
+        self.segmentation.split_all_combined_files_into_wavs()
 
-    network.fit(ds_train, steps_per_epoch=spe, epochs=cnn.n_epochs,
-              validation_data=ds_check, validation_steps=len(check_indices),
-              verbose=1)
+        # stamp = seg.wav_to_stamp('num','six','phone_en-UK-m-Martin00.wav')
 
-    score = network.evaluate(ds_check,steps=len(check_indices),verbose=1)
-    print("Score = ", score)
+        self.create_dataset_from_folders('num')
 
-def main():
-    preprocessing()
-    network()
+        self.create_dataset_from_adhoc_wavs('num' + '-test')
 
 
-    
+    def network(self):
+        network = cnn.build_network()
+
+        loss_function = 'categorical_crossentropy'
+        optimizer = keras.optimizers.Adam(lr = 0.001)
+
+        network.compile(loss=loss_function, optimizer=optimizer, metrics=['accuracy'])
+
+        dataset = pickle.load(open(os.path.join('data', self.prefix + '.pkl'),'rb'))
+
+
+        train_indices = [i for i, r in enumerate(dataset['rand']) if r <= 0.9]
+        check_indices = [i for i, r in enumerate(dataset['rand']) if r > 0.9]
+
+
+        ds_train = cnn.make_dataset(dataset, train_indices,n_epochs = cnn.n_epochs, batch_size=cnn.batch_size, seed=100)  # shuffles...
+        ds_check = cnn.make_dataset(dataset, check_indices,n_epochs=cnn.n_epochs, batch_size=1)
+
+        spe = len(train_indices) // cnn.batch_size
+
+        network.fit(ds_train, steps_per_epoch=spe, epochs=cnn.n_epochs,
+                  validation_data=ds_check, validation_steps=len(check_indices),
+                  verbose=1)
+
+        score = network.evaluate(ds_check,steps=len(check_indices),verbose=1)
+        print("Score = ", score)
+
+    def main(self):
+        self.preprocessing()
+        self.network()
+
+
+
 
 if __name__ == "__main__":
-    main()
+    main = Main(Segmentation(), Network())
+    main.main()
